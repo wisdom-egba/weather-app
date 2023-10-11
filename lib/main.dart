83b2ba865db3c36dc4a1785ecbfd715411618262
Wisdom Egba <99222194+wiz-js@users.noreply.github.com>
2023-10-11 14:02:21 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/weather_screen.dart';
+
+void main() {
+  runApp(const WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WeatherScreen(),
+    );
+  }
+}
